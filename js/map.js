53ead33ac5f8aa8fd95a422d236f0f8edf13dedc
Arvindram Krishnamoorthy <karvi90@yahoo.com>
2015-10-18 00:58:12 -0700

    I HAS CIRCLESSSS

--- js/map.js
+++ js/map.js
@@ -1,15 +1,9 @@
-// assign global variable to data
-// go to console to print in global variable
-
-var globalData = "GLOBALDATA";	
-
 // Function to draw your map
 var drawMap = function() {
 // Create map and set view
 	var map = L.map('map').setView([40, -100], 5)
 // Create a tile layer variable using the appropriate url
 	var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 // Execute your function to get data
 	getData(map)
@@ -18,45 +12,63 @@ var drawMap = function() {
 // Function for getting data
 var getData = function(map) {
 // Execute an AJAX request to get the data in data/response.js
-	$.get('data/response.json', function(data) {
-		console.log(globalData);
-		customBuild(data, map);
-	});
-	console.log("successful AJAX");
+	var data;
+	$.ajax({
+		url: 'data/response.json',
+		type: "get",
+		success:function(dat) {
+			data = dat
+			customBuild(data, map);
+		},
+		dataType:"json"
+	})
+
 // When your request is successful, call your customBuild function
 }
 
 // Loop through your data and add the appropriate layers and points
 var customBuild = function(data, map) {
-// Be sure to add each layer to the map
-	var victimArmed = new L.LayerGroup([]);
-	var victimUnarmed = new L.LayerGroup([]);
+	var victimKilled = new L.LayerGroup([]);
+	var victimHit = new L.LayerGroup([]);
+	var armedMale = 0;
+	var unarmedMale = 0;
+	var armedFemale = 0;
+	var unarmedFemale = 0;
 	for(var i = 0; i < data.length; i++) {
-	
-		if (data[i]["Armed or Unarmed"] == "Armed")  {
-			if (data[i]["Hit or Killed"] == "Killed") {
-				var circle = new L.circleMarker([data[i].lat, data[i].lng], 'red');
-			}
-			else {
-				var circle = new L.circleMarker([data[i].lat, data[i].lng], 'black');	
-			}
-			circle.addTo(victimArmed);
+		if (data[i]["Hit or Killed"] == "Killed") {
+			var circle = new L.circleMarker([data[i].lat, data[i].lng], 'red');
+			circle.addTo(victimKilled);
 		}
-		else {  
-			if (data[i]["Hit or Killed"] == "Killed") {
-				var circle = new L.circleMarker([data[i].lat, data[i].lng], 'red');
+		else {
+			var circle = new L.circleMarker([data[i].lat, data[i].lng], 'black');	
+			circle.addTo(victimHit);
+		}
+		
+		circle.bindPopup(data[i]["Summary"]);
+		
+		if (data[i]["Armed or Unarmed"] == "Armed")  { 
+			if (data[i]["Victim's Gender"] == "Male") {
+				armedMale += 1;
 			}
 			else {
-				var circle = new L.circleMarker([data[i].lat, data[i].lng], 'black');	
+				armedFemale += 1;
 			}
-			circle.addTo(victimUnarmed);
-		};
+		}
+		else {
+			if (data[i]["Victim's Gender"] == "Male") {
+				unarmedMale += 1;
+			}
+			else {
+				unarmedFemale += 1;
+			}	
+		}
 	};
-
+	victimKilled.addTo(map);
+	victimHit.addTo(map);
 	var layers = {
-		"Armed Victim": victimArmed,
-		"Unarmed Victim": victimUnarmed
-	}
+		"Killed Victim": victimKilled,
+		"Hit Victim": victimHit
+	};
 	
 	L.control.layers(null, layers).addTo(map);
 }
